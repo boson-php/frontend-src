10be7d96c26c67bc9d15114fd93dc7dea139d560
Kirill Nesmeyanov <nesk@xakep.ru>
2025-12-29 13:43:32 +0300

    Add loop module

--- src/export.hpp
+++ src/export.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #if defined(_WIN32) || defined(__CYGWIN__)
-  #define BOSON_API __declspec(dllexport)
+  #ifdef BOSON_BUILD
+    #define BOSON_API __declspec(dllexport)
+  #else
+    #define BOSON_API __declspec(dllimport)
+  #endif
 #else
   #define BOSON_API __attribute__((visibility("default")))
 #endif
