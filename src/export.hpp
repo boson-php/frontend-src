8d8948d955dee5c63f32945861814f8dc1a56d80
Kirill Nesmeyanov <nesk@xakep.ru>
2025-12-29 13:36:31 +0300

    Try to fix win32 inconsistent linkage

--- src/export.hpp
+++ src/export.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
 #if defined(_WIN32) || defined(__CYGWIN__)
-  #ifdef BOSON_BUILD
-    #define BOSON_API __declspec(dllexport)
-  #else
-    #define BOSON_API __declspec(dllimport)
-  #endif
+  #define BOSON_API __declspec(dllexport)
 #else
   #define BOSON_API __attribute__((visibility("default")))
 #endif
